f6ae481108d6907d59ecb9f2ec00a17d916aad68
samuel albanie <satiricallaughter@gmail.com>
2014-08-01 09:37:41 +0100

    Fixed /polls url

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-#admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
